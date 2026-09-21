2ffbe0ca59a7180f1df2b73f6a26f36549acc7ba
Soundwavesz <astralcode55@gmail.com>
2026-09-21 08:04:05 +0700

    Initial commit - Setup Reach + Tailwind V4

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-slate-900 text-white">
+      <h1 className="text-4xl font-bold text-sky-400">
+        Tailwind CSS Berhasil Aktif! 🚀
+      </h1>
+    </div>
+  )
+}
